b853ccdc870b9d42bd761d90d66cdb72369d5bdd
Sai Kishore Koyalwar <sai_kishore_koyalwar@epam.com>
2022-04-06 20:01:19 +0530

    Added Operator Check for Division

--- Js_datatypesTask/BasicMathematicalOperators.js
+++ Js_datatypesTask/BasicMathematicalOperators.js
@@ -9,7 +9,8 @@ function basicOp(operation, value1, value2)
   } 
   else if(operation == '*'){
     return value1 * value2;
-  }else{
+  }
+  else if(operation == '/'){
   return value1 / value2;  
   }
   
