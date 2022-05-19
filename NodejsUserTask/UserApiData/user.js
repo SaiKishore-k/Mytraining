68f00c3ffe1b73b300779a1ea0a2352586b23815
Sai Kishore Koyalwar <sai_kishore_koyalwar@epam.com>
2022-05-19 18:23:03 +0530

    Node Js Task 2 is Completed

---
+++ NodejsUserTask/UserApiData/user.js
@@ -0,0 +1,29 @@
+const users=[{
+    "id":"1",
+    "login":"sai_kishore",
+    "password":"1234",
+    "age":20,
+    "isDeleted":false
+
+},
+{
+    "id":"2",
+    "login":"_saikishore",
+    "password":"5678",
+    "age":21,
+    "isDeleted":false
+
+},
+{
+    "id":"3",
+    "login":"saikishore_",
+    "password":"91011",
+    "age":22,
+    "isDeleted":false
+
+}
+]
+
+module.exports={
+    users:users
+}
